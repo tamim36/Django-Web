2c6447c8ca60ed735d8c702dbdb959ce4db73617
tamim36 <tamimarefinanik@gmail.com>
2019-03-23 17:57:40 +0600

    finished up with view posts

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
-from . import views 
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    url('^$', views.index, name = 'index')
-];
+    path('', views.index, name = 'index'),
+    path('details/<int:id>/', views.details, name = 'details')
+]
